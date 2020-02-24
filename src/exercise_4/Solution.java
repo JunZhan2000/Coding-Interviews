7e3991ec9d8db08c2ed6aef42ddba79a2de17b7c
e <2327648010@qq.com>
2020-02-24 21:43:28 +0800

    04 05 06 md

--- src/exercise_4/Solution.java
+++ src/exercise_4/Solution.java
@@ -1,20 +1,13 @@
-package exercise_4;
-
+//题目描述
 //输入某二叉树的前序遍历和中序遍历的结果，请重建出该二叉树。
 //假设输入的前序遍历和中序遍历的结果中都不含重复的数字。
 //例如输入前序遍历序列{1,2,4,7,3,5,6,8}和中序遍历序列{4,7,2,1,5,3,8,6}，则重建二叉树并返回。
 
-
-import BinaryTree.BinaryTree;
-
-import java.util.Arrays;
+package exercise_4;
 
 public class Solution {
     public static void main(String[] args){
-//        int[] pre = {10, 6, 4, 8, 14, 12, 16}, in = {4, 6, 8, 10, 12, 14, 16};
-//        TreeNode treeNode = new Solution().reConstructBinaryTree(pre, in);
         TreeNode treeNode1 = new Solution().reConstructBinaryTree(new int[]{1, 2}, new int[]{2, 1});
-
         System.out.println();
     }
 
@@ -61,3 +54,25 @@ public class Solution {
         return treeNode;
     }
 }
+
+//来自牛客网
+//public class Solution {
+//    public TreeNode reConstructBinaryTree(int [] pre,int [] in) {
+//        TreeNode root=reConstructBinaryTree(pre,0,pre.length-1,in,0,in.length-1);
+//        return root;
+//    }
+//    //前序遍历{1,2,4,7,3,5,6,8}和中序遍历序列{4,7,2,1,5,3,8,6}
+//    private TreeNode reConstructBinaryTree(int [] pre,int startPre,int endPre,int [] in,int startIn,int endIn) {
+//
+//        if(startPre>endPre||startIn>endIn)
+//            return null;
+//        TreeNode root=new TreeNode(pre[startPre]);
+//
+//        for(int i=startIn;i<=endIn;i++)
+//            if(in[i]==pre[startPre]){
+//                root.left=reConstructBinaryTree(pre,startPre+1,startPre+i-startIn,in,startIn,i-1);
+//                root.right=reConstructBinaryTree(pre,i-startIn+startPre+1,endPre,in,i+1,endIn);
+//                return root;
+//            }
+//    }
+//}
